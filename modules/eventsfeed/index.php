4eaf907a878fdeca88064de9be5bca382f06e853
Samuel Adu-Berekorang <samueladu1970@gmail.com>
2025-12-07 12:42:35 +0000

    Remove direct access prevention comments in eventsfeed module and streamline cache control headers for improved security and performance.

--- modules/eventsfeed/index.php
+++ modules/eventsfeed/index.php
@@ -1,7 +1,4 @@
 <?php
-/**
- * Security file - prevents direct access
- */
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
 header('Cache-Control: no-store, no-cache, must-revalidate');
